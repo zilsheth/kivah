956188e68d7ddea8f40d8eb05d78f7de69017a0b
Zil Patel <zillpatel330@gmail.com>
2026-07-20 16:05:32 -0400

    Add environment variable for API URL

--- kivah-client/src/lib/api.ts
+++ kivah-client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
