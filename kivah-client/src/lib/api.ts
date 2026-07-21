d6119f6ed4c1b9c112e677d9ab645fe93a624402
Zil Patel <zillpatel330@gmail.com>
2026-07-21 15:54:03 -0400

    Debug API URL

--- kivah-client/src/lib/api.ts
+++ kivah-client/src/lib/api.ts
@@ -7,6 +7,8 @@ const api = axios.create({
   }
 })
 
+console.log('API URL:', import.meta.env.VITE_API_URL)
+
 // Automatically attach JWT token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('kivah_token')
